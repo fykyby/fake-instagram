236ce704085e560cd92b77b366a58c2466e82826
fykyby <iploritpl@gmail.com>
2022-06-30 23:30:43 +0200

    Tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Post from "./Post";
 export default function Home(props) {
   return (
     <div>
-      <main className="bg-gray-200 flex justify-center md:py-2">
+      <main className="min-h-[calc(100vh-3rem)] sm:min-h-[calc(100vh-4rem)] bg-gray-200 flex justify-center md:py-2">
         <div className="flex flex-col gap-2 max-w-3xl">
           {props.posts.map((post, index) => {
             return <Post data={post} key={index} />;
