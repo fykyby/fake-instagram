d5d2c59e8eadd7ccf259d2e482159bbbc65dba53
fykyby <iploritpl@gmail.com>
2022-07-10 18:06:48 +0200

    Fix login screen responsiveness

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 export default function Login(props) {
   return (
-    <div>
-      <main className="min-h-screen flex gap-6 flex-col justify-center items-center">
-        <h1 className="font-grand-hotel text-5xl select-none fixed top-0 py-6">
+    <div className="min-h-screen flex flex-col justify-start items-center">
+      <header className="w-full text-center min-h-[13vh]">
+        <h1 className="font-grand-hotel text-5xl select-none py-6">
           Fake-Instagram
         </h1>
+      </header>
+      <main className="w-full min-h-[75vh] flex flex-col justify-center items-center gap-6">
         <h1 className="text-4xl">Log in to continue</h1>
         <button
           onClick={props.login}
